cbc9705def4dd0b979dadf0d8070259f5da1c902
tsubasa1122 <nakanot@open8.com>
2019-07-15 22:12:18 +0900

    userの登録機能の実装

--- app/models/user.rb
+++ app/models/user.rb
@@ -14,8 +14,16 @@
 #
 
 class User < ApplicationRecord
-  has_secure_password
+  has_secure_password validations: true
 
-  validates :name, presence: true, uniqueness: true, length: { maximum: 16 }, format: { with: /\A[a-z0-9]+\z/, message: 'は小文字英数字で入力してください'}
-  validates :password, length: { minimum: 8 }
+  validates :name,
+    presence: true,
+    uniqueness: true,
+    length: { maximum: 16 },
+    format: {
+      with: /\A[a-z0-9]+\z/,
+      message: 'は小文字英数字で入力してください'
+    }
+  validates :password,
+    length: { minimum: 8 }
 end
