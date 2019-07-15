b03a6fe5828b8bd0676354f570721505cd5b2409
tsubasa1122 <nakanot@open8.com>
2019-07-15 22:28:42 +0900

    userのログイン機能実装

--- Gemfile
+++ Gemfile
@@ -1,7 +1,7 @@
 source 'https://rubygems.org'
 
 
-gem 'rails', '~> 5.0.0', '>= 5.0.0.1'
+gem 'rails', '5.2.2'
 gem 'mysql2', '>= 0.3.18', '< 0.5'
 gem 'puma', '~> 3.0'
 gem 'sass-rails', '~> 5.0'
--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,3 +1,11 @@
 class ApplicationController < ActionController::Base
   protect_from_forgery with: :exception
+  before_action :currrent_user
+
+  private
+
+  def currrent_user
+    return unless session[:user_id]
+    @current_user = User.find_by(id: session[:user_id])
+  end
 end
